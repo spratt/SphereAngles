f03c9e66e8c861604d246136e57956370bfd82fa
Simon Pratt <simon@pr4tt.com>
2014-03-17 00:08:02 -0400

    Complete but buggy

--- angles.py
+++ angles.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3.2
+#!/usr/bin/env python3.3
 
 MIN_MRD = -180.0
 MAX_MRD = 180.0
@@ -53,10 +53,10 @@ if par1 < MIN_PAR or par1 > MAX_PAR or par2 < MIN_PAR or par2 > MAX_PAR:
 ################################################################################
 # Determine the spherical coordinates
 #
-#   par1    par2
+#   mrd1    mrd2
 #    |       |
 #    |       |
-#   -a-------+--mrd1
+#   -a-------+--par1
 #    |\      |
 #    | \     |
 #    |  \    |
@@ -64,28 +64,82 @@ if par1 < MIN_PAR or par1 > MAX_PAR or par2 < MIN_PAR or par2 > MAX_PAR:
 #    |    \  |
 #    |     \ |
 #    |      \|
-#   -b-------c---mrd2
+#   -b-------c---par2
 #    |       |
 #
 
 #    lat   lon
-a = (mrd1, par1)
-b = (mrd2, par1)
-c = (mrd2, par2)
+a = (par1, mrd1)
+b = (par2, mrd1)
+c = (par2, mrd2)
 
 print('a={}, b={}, c={}'.format(a, b, c))
 
 ################################################################################
-# Translate the spherical coordinates into cartesian coordinates
+# Convert the spherical coordinates into cartesian coordinates of three points
+#
+#    p
+#    |\      
+#    | \     
+#    |  \    
+#    |   \   
+#    |    \  
+#    |     \ 
+#    |      \
+#    q-------r
+#
+
 
 def sphereToCartesian(point, radius):
     lat, lon = point[0], point[1]
     return (radius * math.sin(lat) * math.cos(lon),
-            radius * math.sin(lat) * math.sin(lat),
-            radius * math.cos(lat))
+            radius * math.sin(lat) * math.sin(lon),
+            radius * math.cos(lon))
 
 p = sphereToCartesian(a, rad)
 q = sphereToCartesian(b, rad)
 r = sphereToCartesian(c, rad)
 
 print('p={}, q={}, r={}'.format(p, q, r))
+
+################################################################################
+# Convert the points into two vectors with the same origin
+#
+#    u
+#    ,      
+#   /|\      
+#    |      
+#    |      
+#    |      
+#    |      
+#    |      
+#    o------> v
+#
+
+def getVector(p0, p1):
+    """ Gives the 3d vector from p0 to p1 """
+    return (p1[0] - p0[0], p1[1] - p0[1], p1[2] - p0[2])
+
+u = getVector(q, p)
+v = getVector(q, r)
+
+print('u={}, v={}'.format(u, v))
+
+################################################################################
+# Calculate the angle between the vectors
+
+import numpy as np
+
+rad = np.arccos(np.dot(u,v) / np.linalg.norm(u) / np.linalg.norm(v))
+
+print('rad={}'.format(rad))
+
+################################################################################
+# Convert radians to degrees
+
+def radToDeg(angle):
+    return angle * 180.0 / math.pi
+
+deg = radToDeg(rad)
+
+print('deg={}'.format(deg))
